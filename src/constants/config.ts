5e7953976fb9755bb6aa88364b07ae3bcab1d144
David Myeonghan Ryu <myeonghan12@gmail.com>
2025-12-29 00:12:21 +0900

    refactor: env 수정

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,4 +1,4 @@
-export const POKE_BASE_URL = process.env.REACT_APP_POKE_API_URL;
+export const POKE_BASE_URL = import.meta.env.VITE_POKE_API_URL;
 
 if (!POKE_BASE_URL) {
   console.error("base url undefined!");
