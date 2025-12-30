fb9925617db475ec6165abefeb13527e3dea60d7
David Myeonghan Ryu <myeonghan12@gmail.com>
2025-12-31 01:03:31 +0900

    feat: Storybook 설정

---
+++ packages/design-system/.storybook/preview.ts
@@ -0,0 +1,16 @@
+import type { Preview } from "@storybook/react";
+
+import "../src/tokens/colors.scss";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
