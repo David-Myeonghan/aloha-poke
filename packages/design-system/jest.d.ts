7ff8eaeab0f09f2b3dd795932555e03ea703c2b5
David Myeonghan Ryu <myeonghan12@gmail.com>
2026-01-01 21:43:13 +0900

    test: 테스팅 라이브러리 업데이트

--- packages/design-system/jest.d.ts
+++ packages/design-system/jest.d.ts
@@ -1,18 +1,1 @@
 import '@testing-library/jest-dom';
-
-declare global {
-  namespace jest {
-    interface Matchers<R> {
-      toBeInTheDocument(): R;
-      toBeDisabled(): R;
-      toBeEnabled(): R;
-      toHaveClass(...classNames: string[]): R;
-      toHaveAttribute(attr: string, value?: string | RegExp): R;
-      toHaveStyle(css: Record<string, unknown> | string): R;
-      toHaveTextContent(text: string | RegExp): R;
-      toBeVisible(): R;
-      toContainElement(element: HTMLElement | null): R;
-      toHaveValue(value: string | string[] | number): R;
-    }
-  }
-}
