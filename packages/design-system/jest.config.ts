9406d9c35f3926525894b0b129466d802be312ba
David Myeonghan Ryu <myeonghan12@gmail.com>
2026-01-01 21:01:05 +0900

    test: Jest 테스트 환경 설정 추가

---
+++ packages/design-system/jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.module\\.(css|scss)$': 'identity-obj-proxy',
+    '\\.(css|scss)$': 'identity-obj-proxy',
+  },
+  testMatch: ['**/*.test.ts', '**/*.test.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.stories.tsx',
+    '!src/**/index.ts',
+  ],
+};
+
+export default config;
