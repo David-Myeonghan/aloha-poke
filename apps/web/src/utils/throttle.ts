9cd4c821c82ba87aa3dca7713bbb17d1eb6afd6e
David Myeonghan Ryu <myeonghan12@gmail.com>
2026-01-07 00:41:44 +0900

    feat: debounce 추가

--- apps/web/src/utils/throttle.ts
+++ apps/web/src/utils/throttle.ts
@@ -4,11 +4,29 @@ export function throttle<T extends (...args: Parameters<T>) => void>(
 ): (...args: Parameters<T>) => void {
   let lastRun = 0;
 
+  // delay 마다 1번씩 실행 // leading only
   return (...args: Parameters<T>) => {
     const now = Date.now();
     if (now - lastRun >= delay) {
       lastRun = now;
       fn(...args);
     }
+  };
+}
+
+export function debounce<T extends (...args: Parameters<T>) => void>(
+  fn: T,
+  delay: number,
+): (...args: Parameters<T>) => void {
+  let timeoutId: ReturnType<typeof setTimeout> | null = null;
+
+  return (...args: Parameters<T>) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      fn(...args);
+      timeoutId = null;
+    }, delay);
   };
 }
