7bd0aa38df2d67fe0a856abdf9a332bf0ad46b81
David Myeonghan Ryu <myeonghan12@gmail.com>
2026-01-11 22:01:09 +0900

    feat: AdSlot

---
+++ apps/web/src/pages/MainList/ui/AdSlot.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import classNames from "classnames/bind";
+import { Loading, Typography } from "@mydav/design-system";
+
+import styles from "./AdSlot.module.scss";
+
+const cx = classNames.bind(styles);
+
+const AD_TEXTS = [
+  "Catch them all!",
+  "Train your Pokemon today",
+  "Become a Pokemon Master",
+  "Adventure awaits",
+  "Gotta catch 'em all!",
+];
+
+interface AdSlotProps {
+  index: number;
+}
+
+export default function AdSlot({ index }: AdSlotProps) {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const text = AD_TEXTS[index % AD_TEXTS.length];
+
+  return (
+    <div className={cx("ad-slot")}>
+      {!isLoaded && (
+        <div className={cx("loading-container")}>
+          <Loading size="small" />
+        </div>
+      )}
+      <img
+        src={`https://picsum.photos/400/60?random=${index}`}
+        alt=""
+        className={cx("ad-image", { loaded: isLoaded })}
+        loading="lazy"
+        onLoad={() => setIsLoaded(true)}
+      />
+      {isLoaded && (
+        <div className={cx("ad-overlay")}>
+          <Typography size="t3">{text}</Typography>
+          <Typography size="t4">Sponsored</Typography>
+        </div>
+      )}
+    </div>
+  );
+}
